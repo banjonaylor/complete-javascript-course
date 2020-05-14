3b411bcd0ef87ad3796f92078af73110c86db642
BanjoNaylor <benjamin.naylor@mudano.com>
2020-05-14 13:31:24 +0100

    my take on sections 2 adn 3

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -168,8 +168,418 @@ if (bmiMark > bmiJohn) {
 */
 
 /*******************************
- * If / Else statements
+ * Boolean logic
  */
+// var firstName = 'John'
+// var age = 20
+
+// if (age < 13) {
+//     console.log(firstName + ' is a boy');
+// } else if (age >= 13 && age < 20) {
+//     console.log(firstName + ' is a teenager')
+// } else if (age >= 20 && age < 30) {
+//     console.log(firstName + ' is a young man')
+// } else {
+//     console.log(firstName + ' is a man');
+// }
+
+/*******************************
+ * Ternary operator and Switch
+ */
+
+ /*
+var age = 16
+var firstName = 'John'
+
+//ternary operator
+age > 18 ? console.log(firstName + ' drinks beer') 
+: console.log(firstName + ' drinks juice');
+
+var drink  = age > 18 ? 'beer' : 'juice'
+
+console.log(firstName + ' drinks ' + drink)
+
+//Switch statement
+var job = 'cop';
+
+switch (job) {
+    case 'teacher':
+    case 'instructor':
+        console.log(firstName + ' teaches kids how to code');
+        break;
+    case 'driver':
+        console.log(firstName + ' drives an Uber in Lisbon');
+        break;
+    case 'designer':
+        console.log(firstName + ' designs beautiful websites');
+        break;
+    default:
+        console.log(firstName + ' does something else');
+}
+//how to do with with a range rather than simple === 
+switch (true) {
+    case age < 13:
+        console.log(firstName + ' is a boy');
+        break;
+    case age >= 13 && age < 20:
+        console.log(firstName + ' is a teenage');
+        break;
+    case age >= 20 && age < 30:
+        console.log(firstName + ' is a young man');
+        break;
+    default:
+        console.log(firstName + ' is a man');                    
+}
+*/
+
+/*******************************
+ * Falsy Values
+ */
+
+//falsey values = undefined, null, 0, '', NaN
+//these all equal false in a boolean
+
+// truthy values = all values which are not falsey
+// these all equal true in a boolean
+
+/*
+var height;
+height = 23;
+
+if (height || height === 0) {
+    console.log('The variable is defined');
+} else {
+    console.log('The variable has not been defined');
+}
+
+//The or check above is common because 0 is FALSEY and so can trick that it is undefined
+
+// The assignment == does type conversion. The operator === is strict. SO....
+if (height = '23') {
+    console.log('The == operator does type coercion!');
+}
+// Best practice is to use === for strict equals
+*/
+
+/*******************************
+ * Coding Challenge 2
+ */
+/*
+var avgJohnScore = (89 + 120 + 103 )/ 3
+var avgMikeScore = (116 + 94 + 123)/ 3
+var avgMaryScore = (97 + 134 + 105)/ 3
+
+console.log('John: ' + avgJohnScore);
+console.log('Mike: ' + avgMikeScore);
+console.log('Mary: ' + avgMaryScore);
+
+if (avgJohnScore > avgMaryScore && avgJohnScore > avgMikeScore) {
+    console.log('John is the winner');
+} else if (avgMaryScore > avgJohnScore && avgMaryScore > avgMikeScore) {
+    console.log('Mary is the winner');
+} else if (avgMikeScore > avgJohnScore && avgMikeScore > avgMaryScore) {
+    console.log('Mike is the winner');
+} else {
+    console.log('No single player wins');
+}
+*/
+
+/*******************************
+ * Functions
+ */
+/*
+function calculateAge(birthYear) {
+    return 2018 - birthYear;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+
+console.log(ageJohn,ageMike,ageJane);
+
+'Don\'t Repeat Yourself! *DRY* Use functions instead' 
+
+function yearsUntilRetirement(year,firstName) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+    if (retirement > 0) {
+        console.log(firstName + ' retires in ' + retirement + ' years.');
+    } else {
+        console.log(firstName + ' is already retired');
+    }
+}
+
+yearsUntilRetirement(1990,'John');
+yearsUntilRetirement(1948,'Mike');
+yearsUntilRetirement(1969,'Jane');
+*/
+
+/*******************************
+ * Function Statements and Expressions
+ */
+
+/*
+ //function declation
+
+//  function whatDoYouDo(job, firstName) {}
+
+ //function expression
+var whatDoYouDo = function(job,firstName) {
+    switch(job) {
+        case 'teacher':
+            return firstName + ' teaches kids how to code';
+        case 'driver':
+            return firstName + ' drives an Uber in Lisbon';
+        case 'designer':
+            return firstName + ' designs beautiful websites';
+        default: 
+            return firstName + ' has some other job';
+    }
+}
+
+console.log(whatDoYouDo('teacher','John'));
+console.log(whatDoYouDo('driver','Mark'));
+console.log(whatDoYouDo('cop','Mary'));
+*/
+// A JavaScript *expression* always produces a single value 
+// A JavaScript *statment" does not produce an outcome
+/*
+var names = ['John','Mark','Jane'];
+var years = new Array(1990,1969,1948);
+
+console.log(names[0]);
+console.log(names.length);
+
+//mutate array data
+names[1] = 'Ben'
+names[5] = 'Mary'
+names[names.length] = 'Jim' //This adds 1 item
+console.log(names);
+
+//different data types in an array
+
+var john = ['John','Smith',1990,'designer',false];
+john.push('blue'); //append an item
+console.log(john);
+
+john.unshift('Mr') //add new first element
+console.log(john);
+
+john.pop();
+john.pop(); //lose last element
+john.shift(); //lose first element
+console.log(john);
+
+console.log(john.indexOf(1990)); //a result of -1 means that it is not in the array
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John is a designer'
+console.log(isDesigner);
+*/
+/*
+function tipCalculator(bill) {
+    var percentage;
+    if (bill < 50) {
+        percentage = .2
+    } else if (bill < 200) {
+        percentage = .15
+    } else {
+        percentage = .1
+    }
+    return bill * percentage
+}
+
+console.log(tipCalculator(10));
+
+var bills = [124,48,268];
+var tips = [tipCalculator(bills[0]),tipCalculator(bills[1]),tipCalculator(bills[2])];
+var finalValues = [bills[0] + tips[0],bills[1] + tips[1],bills[2] + tips[2]]
+
+console.log(tips, finalValues);
+*/
+
+/*******************************
+ * Objects and properties
+ */
+/*
+ //object literal = use the curly braces
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane','Mark','Bob','Emily'],
+    job: 'teacher',
+    isMarried: false
+}
+console.log(john);
+console.log(john.firstName); //a dot notation can be used to see a property
+console.log(john['lastName']); //this is like accessing something in array by location
+var x = 'birthYear';
+console.log(john[x]);
+
+john.job = 'designer'
+john['isMarried'] = true
+
+console.log(john);
+
+//new object = use the NEW and OBJECT key words
+var jane = new Object();
+jane.firstName = 'Jane';
+jane.birthYear = 1969;
+jane['lastName'] = 'Harley';
+
+console.log(jane);
+*/
+/*******************************
+ * Objects and methods
+ */
+/*
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane','Mark','Bob','Emily'],
+    job: 'teacher',
+    isMarried: false,
+    calcAge: function() {
+        this.age = 2018  - this.birthYear;
+    }
+};
+john.calcAge();
+console.log(john);
+
+var john = {
+    firstName = 'John',
+    lastName = 'Smith',
+    height = 1.59,
+    weight = 78
+}
+var Mark = {
+    firstName = 'Mark',
+    lastName = 'Walhberg',
+    height = 1.34,
+    weight = 84    
+}
+*/
+/*******************************
+ * Loops and Iteration
+ */
+/*
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+} //i++ is i = i + 1
+
+// var john = {
+//     firstName = 'John',
+//     lastName = 'Smith',
+//     birthYear = 1990
+//     job = 'designer',
+//     isMarried = false
+// }
+
+var john = ['John','Smith',1990,'designer',false]
+
+for (var i = 0; i < john.length; i++) {
+    console.log(john[i]);
+}
+
+//this is an equivalent while loop
+var i = 0;
+while (i < john.length) {
+    console.log(john[i]);
+    i++;
+}
+*/
+//continue and break statements
+// var john = ['John','Smith',1990,'designer',false]
+
+// for (var i = 0; i < john.length; i++) {
+//     if (typeof john[i] !== 'string') continue;
+//     console.log(john[i]);
+// }
+
+// for (var i = 0; i < john.length; i++) {
+//     if (typeof john[i] !== 'string') break;
+//     console.log(john[i]);
+// }
+
+//loop backwards
+// for (var i = (john.length)-1; i >= 0; i -= 1) {
+    // if (typeof john[i] !== 'string') break;
+    // console.log(john[i]);
+// }
+
+var john = {
+    fullName: 'John Smith',
+    bills: [124,48,268,180,42],
+    calcTips: function() {
+        this.tips = [];
+        this.finalValues = [];
+        for (var i = 0; i < this.bills.length; i++) 
+        //determine percentage based on tipping rules
+        {
+            var percentage
+            var bill = this.bills[i]
+
+            if (bill < 50) {
+                percentage = .2
+            } else if (bill <= 200) {
+                percentage = .15
+            } else {
+                percentage = .1
+            }
+            //add result to corresponding array
+            this.tips[i] = bill * percentage;
+            this.finalValues[i] = bill + (bill * percentage);
+        }
+    }
+}
+
+function calcAverage(tips) {
+    var sum = 0
+    for (var i = 0; i < tips.length; i++) {
+        sum = sum + tips[i]
+    }
+    return sum / tips.length;
+}
+
+john.calcTips();
+
+
+john.average = calcAverage(john.tips);
+
+console.log(john);
+
+
+
+/*****************************************************************
+ * SECTION 3
+ *  
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,8 +1,19 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+//It works to call a function before it is decleared, because the code is HOISTED before it is executed
+// calcAge(1965);
+// yearsUntilRetire(1983);
 
+// function calcAge(birthYear) {
+//     console.log(2020-birthYear);
+// }
 
+// var yearsUntilRetire = function(birthYear) {
+//     console.log(65-(2020-birthYear));
+// }
+
+// calcAge(1990);
 
 
 
@@ -67,11 +78,36 @@ function third() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+// console.log(this)
+// calcAge(1985)
 
+// function calcAge(birthYear) {
+//     console.log(2020-birthYear);
+//     console.log(this);
+// }
 
+var john = {
+    name: 'John',
+    birthYear: 1990,
+    calcAge: function() {
+        console.log(this);
+        console.log(2020 - this.birthYear);
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+    }
+}
 
+john.calcAge();
 
+var mike  = {
+    name: 'Mike',
+    birthYear: 1984
+};
 
+mike.calcAge = john.calcAge;
 
+mike.calcAge();
 
 
